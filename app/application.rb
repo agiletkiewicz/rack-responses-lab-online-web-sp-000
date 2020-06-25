670dea7aa4c11384c32d2e6bc4650cd6493b2606
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-06-25 19:04:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,9 +5,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    time = Time.new
-    binding.pry
-    if time.localtime.hour <= 12
+    if Time.now.hour <= 12
       resp.write "Good Morning!"
       else
       resp.write "Good Afternoon!"
