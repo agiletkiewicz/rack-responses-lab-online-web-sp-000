a8d4a7a70c7995007a8763104680237d39bcc7d9
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-06-25 18:53:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     
     time = Time.new
     
-    if time.localtime <= 12
+    if time.localtime.hour <= 12
       resp.write "Good Morning!"
       else
       resp.write "Good Afternoon!"
